ef3f5410ff4cb49af99b546c85a178e23dd9266e
Joony <jonathan.mcallister@gmail.com>
2012-03-14 16:43:44 +0100

    Initial import

--- GalleryTest/ViewController.m
+++ GalleryTest/ViewController.m
@@ -10,6 +10,8 @@
 
 @implementation ViewController
 
+@synthesize scrollView;
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -21,6 +23,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UITapGestureRecognizer *singleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+    [singleTapRecognizer setNumberOfTapsRequired:1];
+    [singleTapRecognizer setDelaysTouchesEnded:YES];
+    
+    UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap:)];
+    [doubleTapRecognizer setNumberOfTapsRequired:2];
+    
+    [singleTapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+    
+    [self.scrollView addGestureRecognizer:singleTapRecognizer];
+    [self.scrollView addGestureRecognizer:doubleTapRecognizer];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -33,6 +47,7 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    //[[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
     [super viewWillAppear:animated];
 }
 
@@ -54,7 +69,32 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return YES;
+    return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 }
+
+
+#pragma mark - Gesture Recognizer
+
+- (void)handleTap:(UITapGestureRecognizer *)gestureRecognizer
+{
+    UIApplication *sharedApplication = [UIApplication sharedApplication];
+    BOOL hide = !sharedApplication.statusBarHidden;
+    [sharedApplication setStatusBarHidden:hide withAnimation:UIStatusBarAnimationFade];
+    [UIView beginAnimations:nil context:nil];
+    [self.navigationController.navigationBar setAlpha:hide ? 0.0f : 1.0f];
+    [UIView commitAnimations];
+}
+
+- (void)animationDone:(id)sender
+{
+    
+}
+
+- (void)handleDoubleTap:(UIGestureRecognizer *)gestureRecognizer
+{
+    
+}
+
+
 
 @end
